4d5199a7a580e9cf9589000854ed0d098e706a4d
tom <tom.stromer@ost.ch>
2025-03-26 11:47:48 +0100

    inspect and docker logs

--- src/deployment/containerlab/containerlab.service.go
+++ src/deployment/containerlab/containerlab.service.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"context"
 	"github.com/charmbracelet/log"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
 	"io"
 	"os/exec"
 )
@@ -11,6 +13,8 @@ import (
 type DeploymentProvider interface {
 	Deploy(ctx context.Context, topologyFile string) error
 	Destroy(ctx context.Context, topologyFile string) error
+	Inspect(ctx context.Context, topologyFile string) error
+	streamContainerLogs(ctx context.Context, containerID string) error
 }
 
 type Service struct{}
@@ -39,7 +43,7 @@ func (s *Service) Deploy(ctx context.Context, topologyFile string) error {
 }
 
 func (s *Service) Destroy(ctx context.Context, topologyFile string) error {
-	cmd := exec.CommandContext(ctx, "containerlab", "destroy", "-t", topologyFile)
+	cmd := exec.CommandContext(ctx, "containerlab", "destroy", "-t", topologyFile, "--cleanup")
 
 	stderr, err := cmd.StderrPipe()
 	if err != nil {
@@ -55,6 +59,46 @@ func (s *Service) Destroy(ctx context.Context, topologyFile string) error {
 	}
 	return nil
 }
+func (s *Service) Inspect(ctx context.Context, topologyFile string) error {
+	cmd := exec.CommandContext(ctx, "containerlab", "inspect", "-t", topologyFile)
+
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		return err
+	}
+	if err := cmd.Start(); err != nil {
+		return err
+	}
+	go readOutput(stderr, "stderr")
+
+	if err := cmd.Wait(); err != nil {
+		return err
+	}
+	return nil
+}
+func (s *Service) streamContainerLogs(ctx context.Context, containerID string) error {
+	cli, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		return err
+	}
+	logOptions := container.LogsOptions{
+		ShowStdout: true,
+		ShowStderr: true,
+		Follow:     true,
+		Timestamps: false,
+		Tail:       "all",
+	}
+
+	out, err := cli.ContainerLogs(ctx, containerID, logOptions)
+	if err != nil {
+		return err
+	}
+
+	go readOutput(out, "DockerLogs")
+
+	return nil
+}
+
 func readOutput(pipe io.Reader, pipeName string) {
 	scanner := bufio.NewScanner(pipe)
 	for scanner.Scan() {
